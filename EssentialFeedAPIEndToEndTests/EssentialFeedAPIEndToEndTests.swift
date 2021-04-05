f46a528bfb2f46bfa42e20ad51a066c8e14d0d0d
meoflauta <meoflauta@gmail.com>
2021-04-05 10:57:43 +0800

    Removed empty functions

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -11,14 +11,6 @@ import EssentialFeed
 
 class EssentialFeedAPIEndToEndTests: XCTestCase {
     
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-    
     func test_EndToEndTestServerGETFeedResult_matchesFixedTestAccountData() {
         
         switch getFeedResult() {
