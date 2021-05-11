6a7c4e4e66bd8304f2ae792354fa50ede0ded522
meoflauta <meocodemagnus@gmail.com>
2021-05-11 08:26:45 +0800

    Add ManagedCache and ManagedFeedImage model representations

---
+++ EssentialFeed/FeedCache/ManagedCache+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ManagedCache+CoreDataClass.swift
+//  EssentialFeed
+//
+//  Created by Romeo Flauta on 5/11/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ManagedCache)
+public class ManagedCache: NSManagedObject {
+
+}
---
+++ EssentialFeed/FeedCache/ManagedFeedImage+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ManagedFeedImage+CoreDataClass.swift
+//  EssentialFeed
+//
+//  Created by Romeo Flauta on 5/11/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ManagedFeedImage)
+public class ManagedFeedImage: NSManagedObject {
+
+}
