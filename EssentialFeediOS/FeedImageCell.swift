e7b398c4169051dd0125d19e856bb8ecb620d30b
meoflauta <meocodemagnus@gmail.com>
2021-06-20 14:21:07 +0800

    Render loaded feed on successful load completion

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,19 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeed
+//
+//  Created by Romeo Flauta on 6/20/21.
+//
+
+import UIKit
+
+public class FeedImageCell: UITableViewCell {
+    
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
